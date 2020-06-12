2d25dd301e48afa75b0d786891a90a8b287c0117
chatton <cianhatton@gmail.com>
2020-06-12 13:31:56 +0100

    mereg conflight

--- Assets/scripts/Card.cs
+++ Assets/scripts/Card.cs
@@ -9,7 +9,11 @@ public class Card : MonoBehaviour
 
     [SerializeField] private Color _onMouseEnterColour;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     private Color _originalColor;
+=======
+    private Color originalColor;
+>>>>>>> Stashed changes
 =======
     private Color originalColor;
 >>>>>>> Stashed changes
@@ -20,8 +24,12 @@ public class Card : MonoBehaviour
     {
         _renderer = GetComponent<MeshRenderer>();
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         _originalColor = _renderer.material.color;
         GetComponentInChildren<TextMesh>().text=(cardItem.name + "\n" + "Dmg: " + cardItem.dmg + "\n" + "Cost: " + cardItem.cost);
+=======
+        originalColor = _renderer.material.color;
+>>>>>>> Stashed changes
 =======
         originalColor = _renderer.material.color;
 >>>>>>> Stashed changes
@@ -31,7 +39,11 @@ public class Card : MonoBehaviour
     private void OnMouseExit()
     {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         GetComponent<Renderer>().material.color = _originalColor;
+=======
+        GetComponent<Renderer>().material.color = originalColor;
+>>>>>>> Stashed changes
 =======
         GetComponent<Renderer>().material.color = originalColor;
 >>>>>>> Stashed changes
