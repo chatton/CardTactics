d42731265642059b2ca931ad5b188d6e5c85433c
John Connor <johnkconnor1@gmail.com>
2020-06-12 12:02:10 +0100

    added card item

---
+++ Assets/scripts/CardItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card/New Card")]
+public class CardItem : ScriptableObject {
+    
+        new public string name = "New Card";
+        public Sprite icon = null;
+        public bool isDefaultItem = false;
+}
