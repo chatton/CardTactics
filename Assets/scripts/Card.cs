00fa51412967f587702fa026b47080b157ce016d
chatton <cianhatton@gmail.com>
2020-06-12 12:08:01 +0100

    change colour on hover

--- Assets/scripts/Card.cs
+++ Assets/scripts/Card.cs
@@ -5,5 +5,27 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
 
-  
+
+
+    [SerializeField] private Color _onMouseEnterColour;
+    private Color originalColor;
+    private MeshRenderer _renderer;
+
+
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        originalColor = _renderer.material.color;
+    }
+
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+    private void OnMouseEnter()
+    {
+        _renderer.material.color = _onMouseEnterColour;
+    }
+
 }
