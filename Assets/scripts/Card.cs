89f1ddf4e9098a27ae0f1d442f20760e4aacc879
chatton <cianhatton@gmail.com>
2020-06-12 13:35:49 +0100

    fix bad merge

--- Assets/scripts/Card.cs
+++ Assets/scripts/Card.cs
@@ -8,46 +8,23 @@ public class Card : MonoBehaviour
     public CardItem cardItem;
 
     [SerializeField] private Color _onMouseEnterColour;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    private Color _originalColor;
-=======
-    private Color originalColor;
->>>>>>> Stashed changes
-=======
-    private Color originalColor;
->>>>>>> Stashed changes
+    private Color _originalColour;
     private MeshRenderer _renderer;
 
 
     private void Start()
     {
         _renderer = GetComponent<MeshRenderer>();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        _originalColor = _renderer.material.color;
-        GetComponentInChildren<TextMesh>().text=(cardItem.name + "\n" + "Dmg: " + cardItem.dmg + "\n" + "Cost: " + cardItem.cost);
-=======
-        originalColor = _renderer.material.color;
->>>>>>> Stashed changes
-=======
-        originalColor = _renderer.material.color;
->>>>>>> Stashed changes
+        _originalColour = _renderer.material.color;
+        GetComponentInChildren<TextMesh>().text = (cardItem.name + "\n" + "Dmg: " + cardItem.dmg + "\n" + "Cost: " + cardItem.cost);
     }
 
 
     private void OnMouseExit()
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        GetComponent<Renderer>().material.color = _originalColor;
-=======
-        GetComponent<Renderer>().material.color = originalColor;
->>>>>>> Stashed changes
-=======
-        GetComponent<Renderer>().material.color = originalColor;
->>>>>>> Stashed changes
+        GetComponent<Renderer>().material.color = _originalColour;
     }
+
     private void OnMouseEnter()
     {
         _renderer.material.color = _onMouseEnterColour;
