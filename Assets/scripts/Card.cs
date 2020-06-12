abaaff576d9cbffd1d3f858f1b95a91e0e1f4629
chatton <cianhatton@gmail.com>
2020-06-12 13:31:05 +0100

    mereg conflight

--- Assets/scripts/Card.cs
+++ Assets/scripts/Card.cs
@@ -8,23 +8,22 @@ public class Card : MonoBehaviour
     public CardItem cardItem;
 
     [SerializeField] private Color _onMouseEnterColour;
-    private Color _originalColour;
+    private Color _originalColor;
     private MeshRenderer _renderer;
 
 
     private void Start()
     {
         _renderer = GetComponent<MeshRenderer>();
-        originalColour = _renderer.material.color;
-        GetComponentInChildren<TextMesh>().text=(cardItem.name + "\n" + "Dmg: " + cardItem.dmg + "\n" + "Cost: " + cardItem.cost);  
+        _originalColor = _renderer.material.color;
+        GetComponentInChildren<TextMesh>().text=(cardItem.name + "\n" + "Dmg: " + cardItem.dmg + "\n" + "Cost: " + cardItem.cost);
     }
 
 
     private void OnMouseExit()
     {
-        GetComponent<Renderer>().material.color = _originalColour;
+        GetComponent<Renderer>().material.color = _originalColor;
     }
-
     private void OnMouseEnter()
     {
         _renderer.material.color = _onMouseEnterColour;
---
+++ Assets/scripts/TacticsMove.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TacticsMove : MonoBehaviour {
+
+
+    [SerializeField] private int _movementDistance = 5;
+    
+    // Use this for initialization
+    void Start()
+    {
+        getCurrentTile().walkable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        findPath();
+    }
+
+
+    private void OnMouseOver()
+    {
+        highlightPath();
+    }
+
+    private void highlightPath()
+    {
+        print("Highlighting path!");
+        var path = findPath();
+        foreach (Tile t in path) {
+            t.walkable = true;
+        }
+    }
+
+    Tile getCurrentTile() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit)) {
+       
+            return hit.transform.gameObject.GetComponent<Tile>();
+        }
+        return null;
+    }
+
+    private List<Tile> findPath() {
+
+        Tile startingTile = getCurrentTile();
+        
+        List<Tile> neighbours = startingTile.GetNeighbours();
+        int currentDepth = 0;
+        Queue<Tile> q = new Queue<Tile>();
+        q.Enqueue(startingTile);
+        startingTile.visited = true;
+        startingTile.walkable = true;
+
+        while (q.Count > 0) {
+            Tile t = q.Dequeue();
+            t.visited = true;
+            t.walkable = true;
+
+            if (!t.visited) {
+                var nextNeighbours = t.GetNeighbours();
+                foreach (var n in neighbours) {
+                    q.Enqueue(t);
+                    t.visited = true;
+                    t.walkable = true;
+                }
+            }
+
+        }
+
+        return neighbours;
+    }
+
+}
