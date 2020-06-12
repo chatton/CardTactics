298b80cab551d5d668f08d44ba6a4f53b4239979
John Connor <johnkconnor1@gmail.com>
2020-06-12 12:10:47 +0100

    yes

--- Assets/scripts/CardItem.cs
+++ Assets/scripts/CardItem.cs
@@ -8,4 +8,7 @@ public class CardItem : ScriptableObject {
         new public string name = "New Card";
         public Sprite icon = null;
         public bool isDefaultItem = false;
+        public int dmg;
+        public int cost;
+        public int range;
 }
