9922e862ebeb108790b682b91928d6287bc818f6
chatton <cianhatton@gmail.com>
2020-06-13 12:23:01 +0100

    wip

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,5 +3,33 @@ using System.Collections;
 
 public class Player : TacticsMove
 {
+    public override void BuildPath()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Tile t = hit.collider.GetComponent<Tile>();
+                if (t == null)
+                {
+                    return;
+                }
+
+                if (t.parent == null)
+                {
+                    return;
+                }
+
+                if (t.distance > _movementDistance)
+                {
+                    return;
+                }
+
+                BuildPathFromTile(t);
+            }
+        }
+    }
 }
