72d32d37d820fe3d9a10552df2f4508ef1b0d7a7
John Connor <johnkconnor1@gmail.com>
2020-06-13 15:08:31 +0100

    Boom Boom bOis

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Enemy : TacticsMove
 {
 
     public EnemyStats enemyStats;
-    public int enemyHP;
+    public int enemyHP = 0;
+    public GameObject explosion;
 
 
     private void Start()
     {
         //get HP
-        enemyHP = enemyStats.hitPoint;
+       // enemyHP = enemyStats.hitPoint;
 
         //overwrite distance
         _movementDistance = enemyStats._movementDistance;
@@ -22,15 +24,22 @@ public class Enemy : TacticsMove
         moveSpeed = enemyStats.moveSpeed;
         Debug.Log(_movementDistance);
         Debug.Log(moveSpeed);
-
+        if (enemyHP <= 0)
+        {
+            Explode(transform.position);
+            Destroy(gameObject);
+        }
     }
 
     private void Update()
     {
-        if (enemyHP <=0 )
-        {
-            Destroy(gameObject);
-        }
+
+    }
+
+    public void Explode(Vector3 position)
+    {
+        Instantiate(explosion, position, Quaternion.identity);
+        Debug.Log("BOOOOM");
     }
 
     public override void BuildPath()
